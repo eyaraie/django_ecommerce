8174f220abaa02c997a6c96dfe61218972ff5b4d
Emi Yaraie <emadyaraie@gmail.com>
2023-06-27 18:13:30 +0200

    project added

---
+++ kart/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from store.models import Product
+
+def home(request):
+    products = Product.objects.all().filter(is_available=True)
+
+    # Get the reviews
+    # reviews = None
+    # for product in products:
+    #     reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+
+    context = {
+        'products': products,
+        # 'reviews': reviews,
+    }
+    return render(request, 'home.html', context)
+# def home(request):
+    
+#     return render(request, 'home.html')
